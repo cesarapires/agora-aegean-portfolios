656d15da7ec68db754c320ffca8879ae717a60a6
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-05-17 14:43:11 -0300

    feat: add envs to data base

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,11 @@ type LogLevel = 'TRACE' | 'DEBUG' | 'INFO' | 'WARN' | 'ERROR'
 
 export const env = {
   port: process.env.PORT ?? '3000',
-  logLevel: (process.env.LOG_LEVEL as LogLevel) ?? 'TRACE'
+  logLevel: (process.env.LOG_LEVEL as LogLevel) ?? 'TRACE',
+  dataBase: {
+    url: process.env.DATA_BASE ?? 'mongodb://127.0.0.1:27017',
+    name: process.env.DATA_BASE_NAME ?? 'test',
+    user: process.env.DATA_BASE_USER_NAME ?? '',
+    password: process.env.DATA_BASE_PASSWORD ?? ''
+  }
 }
