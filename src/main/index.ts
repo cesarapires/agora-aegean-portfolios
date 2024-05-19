7e344cc76f46b993bc161de03a7cf3fe029add34
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-05-19 16:16:39 -0300

    feat: add mongoose connect

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,9 +1,13 @@
 import './config/module-alias'
 
 import 'reflect-metadata'
+import mongoose from 'mongoose'
 import * as log from 'loglevel'
 
 import { app } from '@/main/config/app'
 import { env } from '@/main/config/env'
 
-app.listen(env.port, () => { log.info(`Server running at http://localhost:${env.port}`) })
+app.listen(env.port, () => {
+  void mongoose.connect(`${env.dataBase.url}/${env.dataBase.name}`)
+  log.info(`Server running at http://localhost:${env.port}`)
+})
