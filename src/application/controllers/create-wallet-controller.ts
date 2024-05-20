ae5c7f69d9f0b221ba29f881ee7a6e1e5c6627eb
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-05-19 22:37:35 -0300

    chore: add logs to audit

--- src/application/controllers/create-wallet-controller.ts
+++ src/application/controllers/create-wallet-controller.ts
@@ -2,6 +2,7 @@ import { type CreateWallet } from '@/domain/feature/create-wallet'
 import { ok, type HttpResponse } from '../helpers/http'
 import { createWalletSchemaValidation } from '../validation/wallet-schema'
 import { Controller } from './controller'
+import * as log from 'loglevel'
 
 type HttpRequest = { name: string, userId: string, balance: number }
 type Model = Error | {
@@ -15,6 +16,7 @@ export class CreateNewWalletController extends Controller {
 
   async perform (httpRequest: HttpRequest): Promise<HttpResponse<Model>> {
     const id = await this.createNewWallet.handle({ ...httpRequest, creationDate: new Date() })
+    log.info(`New wallet created with ID: ${id}`)
     return ok({ id })
   }
 }
--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -1,11 +1,18 @@
+import { faker } from '@faker-js/faker'
 import { type Router } from 'express'
+import * as log from 'loglevel'
 
 export default (router: Router): void => {
   router.post('/login', (req, res) => {
     const { username, password } = req.body
 
     if (username === 'example' && password === 'password') {
-      res.status(200).json({ message: 'Login successful' })
+      log.info(`Successful login for user: ${username}`)
+      res.status(200).json({
+        access_token: faker.string.uuid(),
+        token_type: 'GenericToken',
+        expires: 3600
+      })
     } else {
       res.status(401).json({ message: 'Invalid credentials' })
     }
