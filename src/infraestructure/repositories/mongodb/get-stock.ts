8fd42e3bd3faa89a6cccc3faab6f86f0b227365c
Cesar Pires <cesar.augusto.pires@hotmail.com>
2024-06-06 11:50:21 -0300

    feat(transaction): create stocks repository (get)

---
+++ src/infraestructure/repositories/mongodb/get-stock.ts
@@ -0,0 +1,31 @@
+import { type StockData, type GetStock } from '@/domain/contracts/repositories/stock'
+import { stockShema } from '@/infraestructure/repositories/mongodb/schema/stock'
+import { type MongoStock } from '@/infraestructure/repositories/mongodb/entities/stock'
+
+import mongoose, { type Model } from 'mongoose'
+
+export class MongoStockRepository implements GetStock {
+  private readonly stock: Model<MongoStock>
+
+  constructor () {
+    this.stock = mongoose.model('Stock', stockShema)
+  }
+
+  async get (stockId: string): Promise<StockData | undefined> {
+    const stock = await this.stock.findById(stockId)
+    if (stock != null) {
+      return {
+        id: stock._id.toHexString(),
+        stock: stock.stock,
+        name: stock.name,
+        close: stock.close,
+        change: stock.change,
+        volume: stock.volume,
+        marketCap: stock.marketCap,
+        logo: stock.logo,
+        sector: stock.sector,
+        type: stock.type
+      }
+    }
+  }
+}
---
+++ tests/infraestructure/repositories/mongodb/get-stock.spec.ts
@@ -0,0 +1,52 @@
+import mongoose, { type Model } from 'mongoose'
+import { type MongoMemoryServer } from 'mongodb-memory-server'
+import { type MongoStock } from '@/infraestructure/repositories/mongodb/entities/stock'
+import { stockShema } from '@/infraestructure/repositories/mongodb/schema/stock'
+import { MongoStockRepository } from '@/infraestructure/repositories/mongodb/get-stock'
+import { makeFakeDb } from './mocks/connection'
+
+describe('MongoStockRepository', () => {
+  let stock: any
+  let sut: MongoStockRepository
+  let mongodb: MongoMemoryServer
+  let mongoStockRepository: Model<MongoStock>
+
+  beforeAll(async () => {
+    mongodb = await makeFakeDb()
+    mongoStockRepository = mongoose.model('Stock', stockShema)
+    stock = {
+      _id: new mongoose.Types.ObjectId(),
+      stock: 'PETR4',
+      name: 'Petrobras PN',
+      close: 28.35,
+      change: -0.45283019,
+      volume: 45783000,
+      marketCap: 383600000000,
+      logo: 'https://example.com/petrobras_logo.svg',
+      sector: 'Energy',
+      type: 'stock'
+    }
+  })
+
+  beforeEach(() => {
+    sut = new MongoStockRepository()
+  })
+
+  afterAll(async () => {
+    await mongoose.disconnect()
+    await mongodb.stop()
+  })
+
+  describe('GetStock', () => {
+    it('should get stock', async () => {
+      const stockCreated = await mongoStockRepository.create(stock)
+
+      delete stock._id
+      stock.id = stockCreated._id.toHexString()
+
+      const stockObtained = await sut.get(stockCreated._id.toHexString())
+
+      expect(stockObtained).toEqual(stock)
+    })
+  })
+})
